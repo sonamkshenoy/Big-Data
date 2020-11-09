14c1bde7d45eca5deb6beb01eb571d75c1cf235f
sonamkshenoy <sonamthemiracle@gmail.com>
2020-11-09 11:30:06 +0530

    Edits in task 2 specs - final modifications

--- Image Analysis using Spark/countryCount.py
+++ Image Analysis using Spark/countryCount.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from __future__ import print_function
 
 import sys
@@ -8,7 +7,7 @@ from pyspark.sql import SparkSession
 if __name__ == "__main__":
 
     if len(sys.argv) < 5:
-        print("Usage: task1.py <word> <stroke> <dataset1> <dataset2>", file=sys.stderr)
+        print("Usage: task2.py <word> <stroke> <dataset1> <dataset2>", file=sys.stderr)
         sys.exit(-1)
 
     spark = SparkSession\
@@ -18,38 +17,32 @@ if __name__ == "__main__":
 
     givenWord = sys.argv[1]
     maxStrokes = int(sys.argv[2])
-  
+
 
     lines_dataset1 = spark.read.option("header", True).csv(sys.argv[3])
     lines_dataset2 = spark.read.option("header", True).csv(sys.argv[4])
 
     combined_dataset = lines_dataset1.join(lines_dataset2, lines_dataset1.key_id == lines_dataset2.key_id).drop(lines_dataset1.word)
 
-    filtered_dataset_unrecognized = combined_dataset[(combined_dataset["word"] == givenWord) & (combined_dataset['recognized']==False)]
-
-    unrecognized_avg = filtered_dataset_unrecognized.agg({"Total_Strokes":"sum"}).collect()
-    unrecognized_dict = {}
-
-    for i in unrecognized_avg:
-        unrecognized_dict.update(i.asDict())
-
-    if(unrecognized_dict['sum(Total_Strokes)'] and unrecognized_dict['sum(Total_Strokes)'] < maxStrokes):
-        count = filtered_dataset_unrecognized.groupby('countrycode').agg({'countrycode':'count'}).collect()
+    filtered_dataset_unrecognized = combined_dataset[(combined_dataset["word"] == givenWord) & (combined_dataset['recognized']==False) & (combined_dataset["Total_Strokes"] < maxStrokes)]
 
 
-        countryCount = {}
-        for i in count:
-            countryCount[i.asDict()['countrycode']] = i.asDict()['count(countrycode)']
+    count = filtered_dataset_unrecognized.groupby('countrycode').agg({'countrycode':'count'}).collect()
 
-        finalCount = {k:v for k,v in sorted(countryCount.items(), key=lambda item: item[0])}
 
+    countryCount = {}
+    for i in count:
+       countryCount[i.asDict()['countrycode']] = i.asDict()['count(countrycode)']
+
+    finalCount = {k:v for k,v in sorted(countryCount.items(), key=lambda item: item[0])}
+
+    if(finalCount):
         for i in finalCount:
-            print(i, finalCount[i], sep=",") # Also with 5 decimals?
-
+            if(finalCount[i]>0):
+                print("%s,%0.5f"%(i, finalCount[i]))
 
     else:
-        # What should we do?
-        print("Reached here?")
-        pass
+        print("%0.5f"%(0))
 
     spark.stop()
+
--- Image Analysis using Spark/wordCount.py
+++ Image Analysis using Spark/wordCount.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python3
 from __future__ import print_function
 
 import sys
 
-# from pyspark.sql.functions import col, avg
 from pyspark.sql import SparkSession
-
 if __name__ == "__main__":
 
     if len(sys.argv) < 4:
@@ -25,15 +22,9 @@ if __name__ == "__main__":
     filtered_dataset_recognized = lines_dataset2[(lines_dataset2['word']==givenWord) & (lines_dataset2['recognized']==True)]
     filtered_dataset_unrecognized = lines_dataset2[(lines_dataset2['word']==givenWord) & (lines_dataset2['recognized']==False)]
 
-    # filtered_dataset_recognized.show()
-    # filtered_dataset_unrecognized.show()
-
-
-    # Calculate average
     recognized_avg = filtered_dataset_recognized.agg({"Total_Strokes":"avg"}).collect()
     recognized_dict = {}
 
-    # Step 1 to extract value of average
     for i in recognized_avg:
         recognized_dict.update(i.asDict())
 
@@ -43,9 +34,8 @@ if __name__ == "__main__":
     for i in unrecognized_avg:
         unrecognized_dict.update(i.asDict())
 
-    # if-else required in case the word isn't present in the csv - shouldn't throw error for None type
     if(recognized_dict['avg(Total_Strokes)']):
-        print("%0.5f"%(recognized_dict['avg(Total_Strokes)'])) # Step 2 to extract value of average
+        print("%0.5f"%(recognized_dict['avg(Total_Strokes)']))
     else:
         print("%0.5f"%0)
 
